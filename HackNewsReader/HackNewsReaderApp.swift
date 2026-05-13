a845fd09a749b5ccc1f153af829b5442e9e108e6
kartikey Agarwal <kartikeyeagarwal1234@gmail.com>
2026-05-13 10:57:36 +0530

    Initial Commit

---
+++ HackNewsReader/HackNewsReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackNewsReaderApp.swift
+//  HackNewsReader
+//
+//  Created by REAL  on 13/05/26.
+//
+
+import SwiftUI
+
+@main
+struct HackNewsReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
